5e49e3c56c7dca04cc2570aee6c21d8e84ee7d14
markus schnalke <meillo@marmaro.de>
2010-07-07 13:34:19 +0200

    mail_dir defaults to /var/mail now hence one usually does not need to specify it in masqmail.conf

--- src/conf.c
+++ src/conf.c
@@ -434,6 +434,7 @@ read_conf(gchar * filename)
 	conf.max_msg_size = 0; /* no limit on msg size */
 	conf.spool_dir = SPOOL_DIR;
 	conf.log_dir = LOG_DIR;
+	conf.mail_dir = "/var/mail";
 
 	if ((in = fopen(filename, "r")) == NULL) {
 		fprintf(stderr, "could not open config file %s: %s\n", filename, strerror(errno));
