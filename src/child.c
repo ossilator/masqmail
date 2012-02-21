f38a40d825d706b0511d24bfff3035c2d5a845f2
markus schnalke <meillo@marmaro.de>
2012-02-21 16:11:00 +0100

    Various minor refactoring.

--- src/child.c
+++ src/child.c
@@ -41,26 +41,28 @@ child(const char *command)
 		pid = fork();
 		if (pid == -1) {
 			return -1;
+
 		} else if (pid == 0) {
 			/* child */
+			char *argv[] = { "/bin/sh", "-c", (char *)command,
+					NULL };
 			int i, max_fd = sysconf(_SC_OPEN_MAX);
+
 			dup2(pipe[0], 0);
 			dup2(pipe[0], 1);
 			dup2(pipe[0], 2);
 
-			if (max_fd <= 0)
+			if (max_fd <= 0) {
 				max_fd = 64;
-			for (i = 3; i < max_fd; i++)
-				close(i);
-
-			{
-				char *argv[] = { "/bin/sh", "-c",
-						(char *) command, NULL };
-				execve(*argv, argv, NULL);
 			}
+			for (i = 3; i < max_fd; i++) {
+				close(i);
+			}
+			execve(*argv, argv, NULL);
 			logwrite(LOG_ALERT, "execve failed: %s\n",
 					strerror(errno));
 			_exit(1);
+
 		} else {
 			/* parent */
 			close(pipe[0]);
