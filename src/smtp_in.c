8840aa9c3e63367d6b3061e4a2ddc44881cbb878
Oswald Buddenhagen <oswald.buddenhagen@gmx.de>
2024-02-26 22:47:12 +0100

    wire up parse_error
    actually include it in error messages.

--- src/smtp_in.c
+++ src/smtp_in.c
@@ -210,7 +210,7 @@ smtp_in(FILE *in, FILE *out, gchar *remote_host)
 	
 				addr = create_address(buf, A_RFC821, remote_host ? NULL : conf.host_name);
 				if (!addr) {
-					smtp_printf(out, "501 %s: syntax error.\r\n", buf);
+					smtp_printf(out, "501 %s: %s\r\n", buf, parse_error);
 				} else if (!addr->domain) {
 					smtp_printf(out, "501 return path must be qualified.\r\n", buf);
 					destroy_address(addr);
@@ -242,7 +242,7 @@ smtp_in(FILE *in, FILE *out, gchar *remote_host)
 	
 				addr = create_address(buf, A_RFC821, remote_host ? NULL : conf.host_name);
 				if (!addr) {
-					smtp_printf(out, "501 %s: syntax error in address.\r\n", buf);
+					smtp_printf(out, "501 %s: %s\r\n", buf, parse_error);
 					break;
 				}
 				if (!addr->domain) {
